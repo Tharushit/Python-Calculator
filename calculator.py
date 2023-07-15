5ddb811dfb58002a58421e5e4859e4ac1c792217
Tharushi Kuruwitage <thathsaraneekt.20@uom.lk>
2023-07-15 12:57:07 +0530

    Logic changed and updated the code

--- calculator.py
+++ calculator.py
@@ -1,36 +1,32 @@
 previous_operations=[]
 
-def add(a, b):
-    return float(a + b)
+def add(a,b):
+  return a+b
+  
+def subtract(a,b):
+  return a-b
+  
+def multiply (a,b):
+  return a*b
 
-def subtract(a, b):
-    return float(a - b)
-
-def multiply(a, b):
-    return float(a * b)
-
-def divide(a, b):
-    if b != 0:
-        return float(a / b)
-    else:
-        print("float division by zero")
-        return None
-    
-def power(a, b):
-    return a ** b
-
-def remainder(a, b):
-    return a % b
-
+def divide(a,b):
+  try:
+    return a/b
+  except Exception as e:
+    print(e)
+def power(a,b):
+  return a**b
+  
+def remainder(a,b):
+  return a%b
+  
 def history():
     if len(previous_operations) == 0:
         print("No past calculations to show")
     else:
         for i, operation in enumerate(previous_operations, start=1):
             print(f"{operation}")
-
-
-
+  
 def select_op(choice):
   if (choice == '#'):
     return -1
@@ -92,65 +88,25 @@ def select_op(choice):
     previous_operations.append(last_calculation)
     print(last_calculation )
     
-    
   else:
-     print("Unrecognized operation")
-     return None
+    print("Unrecognized operation")
     
-
 while True:
-    print("Select operation.")
-    print("1.Add      : + ")
-    print("2.Subtract : - ")
-    print("3.Multiply : * ")
-    print("4.Divide   : / ")
-    print("5.Power    : ^ ")
-    print("6.Remainder: % ")
-    print("7.Terminate: # ")
-    print("8.Reset    : $ ")
-    print("9.History  : ? ")
-
-    choice = input("Enter choice(+,-,*,/,^,%,#,$): ")
-    print(choice)
-
-    if choice == '#':
-        print("Done. Terminating")
-        break
-
-    elif choice == '$':
-        print("Resetting")
-        continue
-    
-    num1 = input("Enter first number: ")
-    if num1 == '#' or num1 == '':
-        print(num1)
-        print("Done. Terminating")
-        break
-    
-    try:
-        num1 = int(num1)
-        print(num1)
-    except ValueError:
-        print(num1)
-        continue
-
-    num2 = input("Enter second number: ")
-    if num2 == '#' or num2 == '':
-        print(num2)
-        print("Done. Terminating")
-        break
-
-    try:
-        num2 = int(num2)
-        print(num2)
-    except ValueError:
-        print(num2)
-        continue
-
-
-    op_func = select_op(choice)
-    if op_func is None:
-        break
-
-    result = op_func(num1, num2)
-    print(f"{num1:.1f} {choice} {num2:.1f} = {result}")
+  print("Select operation.")
+  print("1.Add      : + ")
+  print("2.Subtract : - ")
+  print("3.Multiply : * ")
+  print("4.Divide   : / ")
+  print("5.Power    : ^ ")
+  print("6.Remainder: % ")
+  print("7.Terminate: # ")
+  print("8.Reset    : $ ")
+  print("8.History  : ? ")
+  
+  # take input from the user
+  choice = input("Enter choice(+,-,*,/,^,%,#,$,?): ")
+  print(choice)
+  if(select_op(choice) == -1):
+    #program ends here
+    print("Done. Terminating")
+    exit()
